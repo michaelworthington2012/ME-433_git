c83c0fe49b3868ab8998dbef99e5fc149fe389d4
michaelworthington2012 <michaelworthington2012@U.northwestern.edu>
2016-06-09 18:57:36 -0500

    more updates for final commit

---
+++ Michael_Worthington_ME433-master/Homework15/PIC_code_for_final/nbproject/Makefile-variables.mk
@@ -0,0 +1,20 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# pic32mx795_pim_e16_int_dyn configuration
+CND_ARTIFACT_DIR_pic32mx795_pim_e16_int_dyn=dist/pic32mx795_pim_e16_int_dyn/production
+CND_ARTIFACT_NAME_pic32mx795_pim_e16_int_dyn=PIC_code_for_final.production.hex
+CND_ARTIFACT_PATH_pic32mx795_pim_e16_int_dyn=dist/pic32mx795_pim_e16_int_dyn/production/PIC_code_for_final.production.hex
+CND_PACKAGE_DIR_pic32mx795_pim_e16_int_dyn=${CND_DISTDIR}/pic32mx795_pim_e16_int_dyn/package
+CND_PACKAGE_NAME_pic32mx795_pim_e16_int_dyn=piccodeforfinal.tar
+CND_PACKAGE_PATH_pic32mx795_pim_e16_int_dyn=${CND_DISTDIR}/pic32mx795_pim_e16_int_dyn/package/piccodeforfinal.tar
+# pic32mz_ef_sk_int_dyn configuration
+CND_ARTIFACT_DIR_pic32mz_ef_sk_int_dyn=dist/pic32mz_ef_sk_int_dyn/production
+CND_ARTIFACT_NAME_pic32mz_ef_sk_int_dyn=PIC_code_for_final.production.hex
+CND_ARTIFACT_PATH_pic32mz_ef_sk_int_dyn=dist/pic32mz_ef_sk_int_dyn/production/PIC_code_for_final.production.hex
+CND_PACKAGE_DIR_pic32mz_ef_sk_int_dyn=${CND_DISTDIR}/pic32mz_ef_sk_int_dyn/package
+CND_PACKAGE_NAME_pic32mz_ef_sk_int_dyn=piccodeforfinal.tar
+CND_PACKAGE_PATH_pic32mz_ef_sk_int_dyn=${CND_DISTDIR}/pic32mz_ef_sk_int_dyn/package/piccodeforfinal.tar
